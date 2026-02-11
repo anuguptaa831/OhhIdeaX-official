5eb47886bb641848a6df2042a8e7f31ebed37a74
Anushka Gupta <anugupaa831@gmail.com>
2026-02-11 23:19:10 +0530

    google

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ db.connect((err) => {
 
 // --- GOOGLE STRATEGY SETUP ---
 passport.use(new GoogleStrategy({
-    clientID: "process.env.GOOGLE_CLIENT_ID",
-    clientSecret: "process.env.GOOGLE_CLIENT_SECRET",
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:5000/auth/google/callback"
   },
   (accessToken, refreshToken, profile, done) => {
